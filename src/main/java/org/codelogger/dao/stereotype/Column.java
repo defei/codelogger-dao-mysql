b37d7f778b277f706a8f451437a0bcb0256c1997
Deng Defei <dengdefei@gmail.com>
2015-11-17 18:34:10 +0800

    add more condition to Column

--- src/main/java/org/codelogger/dao/stereotype/Column.java
+++ src/main/java/org/codelogger/dao/stereotype/Column.java
@@ -9,5 +9,29 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Column {
 
-  String name();
+  /**
+   * (Optional) The name of the column. Defaults to the property or field name.
+   */
+  String name() default "";
+
+  /**
+   * (Optional) The column length. (Applies only if a string-valued column is
+   * used.)
+   */
+  int length() default 255;
+
+  /**
+   * (Optional) Whether the database column is nullable.
+   */
+  boolean nullable() default true;
+
+  /**
+   * (Optional) Whether the column is a unique key. This is a shortcut for the
+   * <code>UniqueConstraint</code> annotation at the table level and is useful
+   * for when the unique key constraint corresponds to only a single column.
+   * This constraint applies in addition to any constraint entailed by primary
+   * key mapping and to constraints specified at the table level.
+   */
+  boolean unique() default false;
+
 }
